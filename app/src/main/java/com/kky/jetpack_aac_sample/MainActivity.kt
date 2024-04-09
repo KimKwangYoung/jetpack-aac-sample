c7b7293225c3a5c5e9ad6a66df0b66917e69c944
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-04-09 10:24:27 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/kky/jetpack_aac_sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kky.jetpack_aac_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
