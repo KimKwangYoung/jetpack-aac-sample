8413d2cb79bc004d77d317f339a301c37cb92146
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-04-24 20:10:43 +0900

    feat : TodoRepository 메서드 추가
    - Todo 추가 메서드
    - Todo 삭제 메서드

--- domain/src/main/java/com/kky/domain/repository/TodoRepository.kt
+++ domain/src/main/java/com/kky/domain/repository/TodoRepository.kt
@@ -1,5 +1,10 @@
 package com.kky.domain.repository
 
+import com.kky.domain.model.Todo
+import kotlinx.coroutines.flow.Flow
+
 interface TodoRepository {
-    suspend fun getTodo(tag: String?)
+    suspend fun getTodo(tag: String?): Flow<List<Todo>>
+    suspend fun addTodo(todo: Todo)
+    suspend fun deleteTodo(todo: Todo)
 }
