fe969962f01c99647dd7796ee7c8948e3665033f
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-04-24 20:09:56 +0900

    feat : Room Database 데이터 Coroutine Flow 방식으로 변경

--- data/src/main/java/com/kky/data/local/AppDatabaseDao.kt
+++ data/src/main/java/com/kky/data/local/AppDatabaseDao.kt
@@ -3,12 +3,13 @@ package com.kky.data.local
 import androidx.room.Dao
 import androidx.room.Query
 import com.kky.data.model.TodoEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 internal interface AppDatabaseDao {
     @Query("SELECT * FROM TodoEntity")
-    suspend fun getAllTodoList(): List<TodoEntity>
+    suspend fun getAllTodoList(): Flow<List<TodoEntity>>
 
     @Query("SELECT * FROM TodoEntity WHERE :tag = tag")
-    suspend fun getTodoByTag(tag: String): List<TodoEntity>
+    suspend fun getTodoByTag(tag: String): Flow<List<TodoEntity>>
 }
