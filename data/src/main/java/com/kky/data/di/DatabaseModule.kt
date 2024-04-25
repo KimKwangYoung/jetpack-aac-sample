737b907bf38ea64ea9c9259a9c1a9687ea8e1c13
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-04-25 17:29:58 +0900

    chore : hilt 모듈 singleton 어노테이션 추가

--- data/src/main/java/com/kky/data/di/DatabaseModule.kt
+++ data/src/main/java/com/kky/data/di/DatabaseModule.kt
@@ -7,12 +7,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DatabaseModule {
 
     @Provides
+    @Singleton
     fun provideAppDatabase(
         @ApplicationContext context: Context
     ): AppDatabase {
--- data/src/main/java/com/kky/data/di/RepositoryModule.kt
+++ data/src/main/java/com/kky/data/di/RepositoryModule.kt
@@ -6,10 +6,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RepositoryModule {
     @Binds
+    @Singleton
     abstract fun bindsTodoRepository(impl: TodoRepositoryImpl): TodoRepository
 }
