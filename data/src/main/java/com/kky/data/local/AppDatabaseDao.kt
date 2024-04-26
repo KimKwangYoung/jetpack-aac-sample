9c26e07adf9821e971de895228cb0f6620a256ad
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-04-26 20:59:13 +0900

    feat : Room Database dao 할일, 태그 --> 삽입, 삭제 메소드 추가

--- data/src/main/java/com/kky/data/local/AppDatabaseDao.kt
+++ data/src/main/java/com/kky/data/local/AppDatabaseDao.kt
@@ -1,8 +1,13 @@
 package com.kky.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import com.kky.data.model.TagEntity
+import com.kky.data.model.TaskEntity
 import com.kky.data.model.TodoEntity
+import com.kky.domain.model.Todo
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -12,4 +17,16 @@ internal interface AppDatabaseDao {
 
     @Query("SELECT * FROM TodoEntity WHERE :tag = tag")
     suspend fun getTodoByTag(tag: String): Flow<List<TodoEntity>>
+
+    @Insert
+    suspend fun insertTask(taskEntity: TaskEntity)
+
+    @Delete
+    suspend fun deleteTask(todo: TaskEntity)
+    @Insert
+    suspend fun insertTag(tag: TagEntity)
+
+    @Delete
+    suspend fun deleteTag(tag: TagEntity)
+
 }
